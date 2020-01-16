7556a264aafb3bc87a7ad7d99e97b6818bc109e1
Robert Zondervan <rjzondervan@gmail.com>
2020-01-16 15:35:01 +0100

    test bash script for creating prs

---
+++ create_pr.bash
@@ -0,0 +1,19 @@
+#/bin/bash
+
+export $globalowner=rjzondervan
+export $owner=rjzondervan
+export $repository=testgeneratedcomponent1
+export $branch=master
+export $RELEASE_NAME=V0.1
+export $message="update from proto component"
+
+git remote add child
+git branch update-$RELEASE_NAME
+git checkout update-$RELEASE_NAME
+if[$owner==$globalowner];then
+    git push -u child update-$RELEASE_NAME
+else
+
+fi
+hub pull-request -b $owner/$repository:$branch -h $globalowner/$repository:update-$RELEASE_NAME -m $message
+git remote remove child

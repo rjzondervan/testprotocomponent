169fa5d8e0d8c105656c62c97869513a82a1e60e
Robert Zondervan <rjzondervan@gmail.com>
2020-01-16 15:43:42 +0100

    No dollarsigns in export...

--- create_pr.sh
+++ create_pr.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
-export $globalowner="rjzondervan"
-export $owner="rjzondervan"
-export $repository="testgeneratedcomponent1"
-export $branch="master"
-export $RELEASE_NAME="V0.1"
-export $message="update from proto component"
+export globalowner="rjzondervan"
+export owner="rjzondervan"
+export repository="testgeneratedcomponent1"
+export branch="master"
+export RELEASE_NAME="V0.1"
+export message="update from proto component"
 
 git remote add child https://github.com/$owner/$repository
 git branch update-$RELEASE_NAME
